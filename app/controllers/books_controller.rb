b6146f5a28e37b85d56eacc8101f1a0246c84798
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-07-09 11:01:13 +0000

    課題途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,19 +5,24 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-   if @book.save
+    if @book.save
+     flash[:notice] = "successfully"
       redirect_to book_path(@book.id)
     else
-      render :new
-    end  
-  end
+     @books =Book.all
+     render :index
+    end
+   end
 
   def index
      @books = Book.all
+     @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.all
+    
   end
 
   def edit
@@ -25,18 +30,27 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+     redirect_to book_path(@book.id)
+    else
+       @books =Book.find(params[:id])
+       render :edit
+    end
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
-  end
+    @book = Book.find(params[:id])
+    if @book.destroy
+    flash[:notice] = "successfully"
+    redirect_to books_path
+    
+   end
+ end
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
