b5019d9d8041e009266ea0a5653d6ae8fd975521
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-08-01 03:30:07 +0000

    UPデート

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'post_image/new'
+  get 'post_image/index'
+  get 'post_image/show'
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  resources :books
